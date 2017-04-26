936659aa92baa3a574e1a34ac4ba2b09ebd0cb35
dolgowmm <dolgowmm@gmail.com>
2017-04-26 16:22:45 +0700

    add methods get, remove, update to site controller

--- src/main/java/ru/unionfreearts/webservice/controller/SiteController.java
+++ src/main/java/ru/unionfreearts/webservice/controller/SiteController.java
@@ -42,4 +42,49 @@ public class SiteController {
         }
         return response;
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<Site> getSiteById(@RequestBody long id){
+        Site site = null;
+        ResponseEntity<Site> response;
+        site = (Site)repository.get(id);
+        if (site != null) {
+            response = new ResponseEntity<Site>(site, HttpStatus.OK);
+        }else{
+            response = new ResponseEntity<Site>(HttpStatus.BAD_REQUEST);
+        }
+        return response;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.DELETE)
+    @ResponseBody
+    public ResponseEntity<Boolean> removeSite(@RequestBody String json){
+        Site site = null;
+        ResponseEntity<Boolean> response;
+        try{
+            site = new ObjectMapper().readValue(json, Site.class);
+            boolean removed = repository.remove(site);
+            response = new ResponseEntity<Boolean>(removed, HttpStatus.OK);
+        }catch (IOException ex){
+            response = new ResponseEntity<Boolean>(HttpStatus.BAD_REQUEST);
+        }
+        return response;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.PUT)
+    @ResponseBody
+    public ResponseEntity<Boolean> updateSite(@RequestBody String json){
+        Site site = null;
+        ResponseEntity<Boolean> response;
+        try{
+            site = new ObjectMapper().readValue(json, Site.class);
+            boolean updated = repository.set(site);
+            response = new ResponseEntity<Boolean>(updated, HttpStatus.OK);
+        }catch (IOException ex){
+            response = new ResponseEntity<Boolean>(HttpStatus.BAD_REQUEST);
+        }
+        return response;
+    }
+
 }
