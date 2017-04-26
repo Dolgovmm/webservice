59123892a7abd739cbaaa494381dda739a9c9d06
dolgowmm <dolgowmm@gmail.com>
2017-04-26 13:51:13 +0700

    add interface repository

---
+++ src/main/java/ru/unionfreearts/webservice/repository/Repository.java
@@ -0,0 +1,19 @@
+package ru.unionfreearts.webservice.repository;
+
+import java.util.List;
+
+/**
+ * Created by Михалыч on 26.04.2017.
+ */
+public interface Repository<T> {
+
+    public boolean add(T entity);
+
+    public List<T> getAll();
+
+    public T get(long id);
+
+    public boolean remove(T entity);
+
+    public boolean set(T entity);
+}
