e36c503494ad979cb06bddc5d7c03146fc93a2c0
dolgowmm <dolgowmm@gmail.com>
2017-04-26 15:13:52 +0700

    refactoring site controller method add

--- src/main/java/ru/unionfreearts/webservice/controller/SiteController.java
+++ src/main/java/ru/unionfreearts/webservice/controller/SiteController.java
@@ -30,16 +30,15 @@ public class SiteController {
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<Site> addSite(@RequestBody String json) {
+    public ResponseEntity<Long> addSite(@RequestBody String json) {
         Site site = null;
-        ResponseEntity<Site> response;
+        ResponseEntity<Long> response;
         try {
             site = new ObjectMapper().readValue(json, Site.class);
             repository.add(site);
-            response = new ResponseEntity<Site>(site, HttpStatus.OK);
+            response = new ResponseEntity<Long>(1l, HttpStatus.OK);
         } catch (IOException e) {
-            Site siteForException = new Site();
-            response = new ResponseEntity<Site>(siteForException, HttpStatus.BAD_REQUEST);
+            response = new ResponseEntity<Long>(2l, HttpStatus.BAD_REQUEST);
         }
         return response;
     }
