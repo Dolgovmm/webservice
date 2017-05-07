408bba4268752cf4137952158d940c156b1af39b
dolgowmm <dolgowmm@gmail.com>
2017-05-07 16:18:00 +0700

    add dbService interface

---
+++ src/main/java/ru/unionfreearts/webservice/dbService/DbService.java
@@ -0,0 +1,19 @@
+package ru.unionfreearts.webservice.dbservice;
+
+import java.util.List;
+
+/**
+ * Created by Михалыч on 30.04.2017.
+ */
+public interface DbService<T> {
+
+    public long add(T entity);
+
+    public T get(long id);
+
+    public List<T> getAll();
+
+    public int remove(long id);
+
+    public int update(T entity);
+}
