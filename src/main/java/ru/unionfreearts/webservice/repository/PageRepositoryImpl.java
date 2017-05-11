effcb2aea25a956017a0bd53bed3f8e0fb96187d
dolgowmm <dolgowmm@gmail.com>
2017-05-11 20:25:34 +0700

    add logging to PageRepositoryImpl

--- src/main/java/ru/unionfreearts/webservice/repository/PageRepositoryImpl.java
+++ src/main/java/ru/unionfreearts/webservice/repository/PageRepositoryImpl.java
@@ -10,25 +10,32 @@ import java.util.List;
  * Created by Михалыч on 08.05.2017.
  */
 public class PageRepositoryImpl implements Repository<Page> {
+	static final Logger logger = LoggerFactory.getLogger(PageRepositoryImpl.class);
+	
     private DbService<Page> dbService = new DbServiceImpl<Page>(Page.class);
 
     public long add(Page entity) {
-        return dbService.add(entity);
+        logger.debug("add Page entity: " + entity.toString());
+		return dbService.add(entity);
     }
 
     public List<Page> getAll() {
+		logger.debug("get all pages from table");
         return dbService.getAll();
     }
 
     public Page get(long id) {
-        return dbService.get(id);
+        logger.debug("get Page entity with id: " + id.toString());
+		return dbService.get(id);
     }
 
     public long remove(Page entity) {
+		logger.debug("remove Page entity: " + entity.toString());
         return dbService.remove(entity);
     }
 
     public long update(Page entity) {
-        return dbService.update(entity);
+        logger.debug("update Page entity: " + entity.toString());
+		return dbService.update(entity);
     }
 }
