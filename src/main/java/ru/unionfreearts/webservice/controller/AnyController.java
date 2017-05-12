5f3c998aa6b3415996b7b4c87d6dff92ccac2971
dolgowmm <dolgowmm@gmail.com>
2017-05-12 21:03:53 +0700

    refactoring AnyController

--- src/main/java/ru/unionfreearts/webservice/controller/AnyController.java
+++ src/main/java/ru/unionfreearts/webservice/controller/AnyController.java
@@ -18,23 +18,15 @@ public class AnyController<T> {
     public long add(Repository repository, String json, Class tClass) throws IOException, HibernateException{
         T entity;
         logger.debug("create empty entity instance");
-        long id = -1l;
-        try {
-            entity = (T) new ObjectMapper().readValue(json, tClass);
-            logger.debug("get value entity instance from json");
-            id = repository.add(entity);
-            logger.debug("add entity instance to repository: " + entity.toString());
-        } catch (IOException ex) {
-            logger.error("IOException on read json " + json + " with messsage: " + ex.getMessage());
-            throw new IOException(ex);
-        } catch (HibernateException ex){
-            logger.error("HibernateException on add entity with messsage: " + ex.getMessage());
-            throw new HibernateException(ex);
-        }
+        long id;
+        entity = (T) new ObjectMapper().readValue(json, tClass);
+        logger.debug("get value entity instance from json");
+        id = repository.add(entity);
+        logger.debug("add entity instance to repository: " + entity.toString());
         return id;
     }
 
-    public T getById(Repository repository, long id){
+    public T getById(Repository repository, long id) throws HibernateException{
         T entity;
         logger.debug("create empty entity instance");
         entity = (T)repository.get(id);
@@ -42,42 +34,30 @@ public class AnyController<T> {
         return entity;
     }
 
-    public List<T> getAll(Repository repository){
+    public List<T> getAll(Repository repository) throws HibernateException{
         List<T> list = repository.getAll();
         return list;
     }
 
-    public long remove(Repository repository, String json, Class tClass){
+    public long remove(Repository repository, String json, Class tClass) throws IOException, HibernateException{
         T entity;
-        logger.debug("create empty Site instance");
+        logger.debug("create empty entity instance");
         long removed;
-        try{
-            entity = (T) new ObjectMapper().readValue(json, tClass);
-            logger.debug("get value Site instance from json");
-            removed = repository.remove(entity);
-            logger.debug("remove site from repository: " + entity.toString());
-        }catch (IOException ex){
-            logger.error("IOException on read json " + json + " with messsage: " + ex.getMessage());
-            removed = -1l;
-            logger.debug("set empty responseEntity and status BAD_REQUEST");
-        }
+        entity = (T) new ObjectMapper().readValue(json, tClass);
+        logger.debug("get value entity instance from json");
+        removed = repository.remove(entity);
+        logger.debug("remove entity from repository: " + entity.toString());
         return removed;
     }
 
-    public long update(Repository repository, String json, Class tClass){
+    public long update(Repository repository, String json, Class tClass) throws IOException, HibernateException{
         T entity;
-        logger.debug("create empty Site instance");
+        logger.debug("create empty entity instance");
         long updated;
-        try{
-            entity = (T) new ObjectMapper().readValue(json, tClass);
-            logger.debug("get value Site instance from json");
-            updated = repository.update(entity);
-            logger.debug("update site on repository: " + entity.toString());
-        }catch (IOException ex){
-            logger.error("IOException on read json " + json + " with messsage: " + ex.getMessage());
-            updated = -1;
-            logger.debug("set empty responseEntity and status BAD_REQUEST");
-        }
+        entity = (T) new ObjectMapper().readValue(json, tClass);
+        logger.debug("get value entity instance from json");
+        updated = repository.update(entity);
+        logger.debug("update entity on repository: " + entity.toString());
         return updated;
     }
 }
